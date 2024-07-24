cd1d846586f1f6caedc4475e16df74543e52fd97
numherilab <numerilab@payhericourt.fr>
2024-07-24 17:20:32 +0200

    zone1
    merik, jour 2

--- data/maps/zone_1/04.lua
+++ data/maps/zone_1/04.lua
@@ -10,15 +10,16 @@
 local map = ...
 local game = map:get_game()
 
--- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
 
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
+function sautepano:on_interaction()
+
+game:start_dialog("saute" , function(answer)
+    if answer == 1 then
+            game:start_dialog("saute oui")
+
+    elseif answer == 2 then
+
+        game:start_dialog("saute non")
 end
-
--- Event called after the opening transition effect of the map,
--- that is, when the player takes control of the hero.
-function map:on_opening_transition_finished()
-
+end)
 end
